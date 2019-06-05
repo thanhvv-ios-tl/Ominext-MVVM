c364adeb0f0886df0a1149e8baebcf5de7fc8439
Thanh  Vu <vuvanthanh.haui.ktpm1@gmail.com>
2019-06-05 16:05:15 +0700

    Initial Commit

---
+++ Ominext-MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ominext-MVVM
+//
+//  Created by Thanh Vu on 6/5/19.
+//  Copyright © 2019 Ominext. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
